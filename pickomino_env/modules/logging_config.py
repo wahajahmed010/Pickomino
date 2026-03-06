2cee7643a536e38ce95bc2475a025a88aa97463d
Jarl <jarl@hoyem.de>
2026-03-06 13:45:35 +0100

    Add comment for Windows log file location
    Updated loguru logger configuration to include a comment about the log file location on Windows.

--- pickomino_env/modules/logging_config.py
+++ pickomino_env/modules/logging_config.py
@@ -9,7 +9,7 @@ try:  # pylint: disable=too-many-try-statements
     from loguru import logger
 
     logger.remove()  # Remove default stderr handler.
-    logger.add(Path(tempfile.gettempdir()) / "pickomino.log", mode="w")
+    logger.add(Path(tempfile.gettempdir()) / "pickomino.log", mode="w")  # On Windows: C:\Users\<username>\AppData\Local\Temp\pickomino.log
 
     def log(message: str) -> None:
         """Log a debug message if loguru is available."""
