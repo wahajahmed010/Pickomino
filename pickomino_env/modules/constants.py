22b56fe2c270fdaa72724bcf211fa3cf38a8e3d7
RobMeetsAI <raithelrobin@gmail.com>
2026-03-30 17:14:59 +0100

    Little fix.

--- pickomino_env/modules/constants.py
+++ pickomino_env/modules/constants.py
@@ -41,7 +41,6 @@ __all__ = [
     "FONT_SMALL",
     "GAME_OVER",
     "GAME_OVER_COLOR",
-    "GREEN",
     "LARGEST_TILE",
     "MAX_BOTS",
     "MIN_ROLLS_FOR_WORM_STRATEGY",
@@ -101,14 +100,14 @@ RENDER_MODE_RGB_ARRAY: Final[str] = "rgb_array"
 
 # Rendering frequency and delay for bots.
 RENDER_FPS: Final[int] = 60  # Frames Per Second.
-RENDER_DELAY: Final[float] = 2
+RENDER_DELAY: Final[float] = 0
 
 # Rendering window dimensions.
 WINDOW_WIDTH: Final[int] = 1000
 WINDOW_HEIGHT: Final[int] = 750
 
 # Game Over alignment
-GAME_OVER: Final[tuple[int, int]] = (400, 150)
+GAME_OVER: Final[tuple[int, int]] = (500, 150)
 GAME_OVER_COLOR: Final[tuple[int, int, int]] = (200, 0, 0)
 SCORES: Final[int] = 80
 FONT_BIG: Final[int] = 80
